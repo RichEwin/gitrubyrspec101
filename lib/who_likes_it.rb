8ea9988e18104534365dd7cab68c7859f156a12e
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2020-11-05 18:45:26 +0000

    GREEN expect(likes(['Jacob', 'Alex])).to eq 'Jacob and Alex likes this'

--- lib/who_likes_it.rb
+++ lib/who_likes_it.rb
@@ -3,6 +3,8 @@ def likes(array)
         "no one likes this"
     elsif array.length == 1
         "#{array[0]} likes this"
+    elsif array.length == 2
+        p "#{array[0]} and #{array[1]} like this"
     end 
 end 
 
