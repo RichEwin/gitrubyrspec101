cdb218858d7f60e3dedb3bf782f949ef9ec67e8d
Jonathan Kelly <jonnydkelly@protonmail.com>
2020-11-07 15:47:39 +0000

    added elsif for 3 names

--- lib/who_likes_it.rb
+++ lib/who_likes_it.rb
@@ -5,5 +5,7 @@ def likes(name)
         "#{name[0]} likes this"
     elsif name.length==2
         "#{name[0]} and #{name[1]} like this"
+    elsif name.length==3
+        "#{name[0]}, #{name[1]} and #{name[2]} like this"
     end
 end
