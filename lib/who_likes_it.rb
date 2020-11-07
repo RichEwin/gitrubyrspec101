fc11e7c1a2fd74c364c10ce90f7494a2aa09b0f6
Jonathan Kelly <jonnydkelly@protonmail.com>
2020-11-07 15:50:40 +0000

    added elsif for more than 3 names

--- lib/who_likes_it.rb
+++ lib/who_likes_it.rb
@@ -7,5 +7,7 @@ def likes(name)
         "#{name[0]} and #{name[1]} like this"
     elsif name.length==3
         "#{name[0]}, #{name[1]} and #{name[2]} like this"
+    else name.length >3
+        "#{name[0]}, #{name[1]} and #{name.length-2} others like this"
     end
 end
