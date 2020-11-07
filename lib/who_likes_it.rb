cbfb5224111ac0e62784afac4f5b0407d1f24b23
Jonathan Kelly <jonnydkelly@protonmail.com>
2020-11-07 15:43:15 +0000

    added elsif for one and twon names

--- lib/who_likes_it.rb
+++ lib/who_likes_it.rb
@@ -1,5 +1,9 @@
 def likes(name)
     if name.length == 0
         "no one likes this"
+    elsif name.length ==1
+        "#{name[0]} likes this"
+    elsif name.length==2
+        "#{name[0]} and #{name[1]} like this"
     end
 end
