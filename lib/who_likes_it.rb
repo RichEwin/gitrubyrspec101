ab3537ab968a3d2a4da49a15f24a885477327f31
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2020-11-08 10:56:27 +0000

    3rd test passing

--- lib/who_likes_it.rb
+++ lib/who_likes_it.rb
@@ -3,5 +3,7 @@ def likes(array)
         "no one likes this"
     elsif array.length == 1
         "#{array[0]} likes this"
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]} like this"
     end 
 end 
